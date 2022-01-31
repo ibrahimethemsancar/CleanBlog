cdd584d43d0176dc6b623f412490018219e84277
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-01-31 12:24:42 +0300

    cleanBlog-project-task-2

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-const express=require('express');
+const express = require('express');
 
-const app =express();
+const app = express();
+const path = require('path');
 
-app.get('/',(req,res)=>{
-    const blog={
-        id:1,
-        title:"Blog title",
-        description:"Blog description"
-    };
-    res.send(blog)
+//TEMPLATE ENGINE
+app.set("view engine","ejs");
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
+// app.use('/', express.static(path.join(__dirname, 'index')));
+
+app.get('/',(res,req)=>{
+    req.render('index')
 })
 
-const port =3000;
-
-
-app.listen(port,()=>{
-    console.log(`Sunucu ${port} portunda başlatıldı.`);
+app.get('/about',(res,req)=>{
+    req.render('about')
 })
+app.get('/add',(res,req)=>{
+    req.render('add_post')
+})
+
+
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
